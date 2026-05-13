961767b2f24f33d3b57a03a6a04ff45bec3e335f
Dhruv Rohilla <dhruvrohilla666@gmail.com>
2026-05-13 19:29:05 +0530

    Fixed CORS problem

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://restro-frontend-8997.onrender.com",
     credentials: true,
   }),
 );
